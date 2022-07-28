55268ae6f7deb93c3e5026c1ac0f755bf09c52d7
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2022-07-28 10:30:25 -0300

    routes added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const router = require("./routes");
 
 const app = express();
 
 app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api", router);
 
 app.get("/", (req, res) => {
     res.send("OK");
