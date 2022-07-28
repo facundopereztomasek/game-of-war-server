a62bb0d78fcc46a295979ee4a770ca6fa1a35621
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2022-07-28 12:04:58 -0300

    cors added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const router = require("./routes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
