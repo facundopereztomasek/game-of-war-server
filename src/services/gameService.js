63e8354d135fab36a67ae120c6c7b2b9f79c9a51
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2022-07-29 14:55:47 -0300

    conways game logic refactor to improve speed

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,14 +1,18 @@
 const willSurvive = (matrix, x, y) => {
-    const liveCell = matrix[y][x] === 1;
-    let neighbors = 0;
-    for (let j = -1; j < 2; j++) {
-        for (let i = -1; i < 2; i++) {
-            if (matrix[j + y] === undefined) continue;
-            if (matrix[j + y][i + x] === undefined) continue;
-            if (i === 0 && j === 0) continue;
-            if (matrix[j + y][i + x] === 1) neighbors++;
-        }
-    }
+    const liveCell = matrix[y][x] === 1 ? 1 : 0;
+    const sliceFromY = Math.max(y - 1, 0);
+    const sliceFromX = Math.max(x - 1, 0);
+    const sliceToY = Math.min(y + 2, matrix.length);
+    const sliceToX = Math.min(x + 2, matrix[0].length);
+
+    const focusedMatrix = matrix
+        .slice(sliceFromY, sliceToY)
+        .map((row) => row.slice(sliceFromX, sliceToX))
+        .flat()
+        .filter((_) => _ === 1);
+
+    const neighbors = focusedMatrix.length - (liveCell ? 1 : 0);
+
     return (neighbors === 2 && liveCell) || neighbors === 3;
 };
 
