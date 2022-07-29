9f90c1d3b73f0d3fc324714c052426abdeaade78
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2022-07-29 12:33:50 -0300

    base64 board tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+const encodeBoard = (board) => {
+    const binaryBytes = board.match(/.{1,8}/g);
+    const bytes = binaryBytes.map((_) => parseInt(_, 2));
+
+    const text = bytes.map((_) => String.fromCharCode(_)).join("");
+
+    let buff = Buffer.from(text);
+    let encodedBoard = buff.toString("base64");
+
+    return encodedBoard;
+};
+
+const decodeBoard = (encodedBoard) => {
+    console.log(encodedBoard);
+    const decoded = Buffer.from(encodedBoard, "base64").toString();
+    const binaryBytes2 = decoded.split("").map((_) => _.charCodeAt());
+    const board = binaryBytes2
+        .map((_) => _.toString(2).padStart(8, 0))
+        .join("");
+
+    console.log(encodedBoard);
+
+    return board;
+};
+
 router
     .get("/", (req, res) => {
         res.json({
@@ -10,14 +35,15 @@ router
     .post("/step", (req, res) => {
         const { body } = req;
         const { width, state } = body.board;
-
-        const matrix = state
+        console.log(state);
+        const matrix = decodeBoard(state)
             .match(new RegExp(`.{1,${width}}`, "g"))
             .map((_) => _.split("").map((_) => Number(_)));
 
         const newMatrix = [];
 
         for (let i = 0; i < width; i++) {
+            console.log(i);
             newMatrix[i] = [];
             for (let j = 0; j < matrix[i].length; j++) {
                 newMatrix[i][j] = matrix[i][j] === 0 ? 1 : 0;
@@ -29,7 +55,7 @@ router
         res.json({
             board: {
                 width,
-                state: newState,
+                state: encodeBoard(newState),
             },
         });
     });
